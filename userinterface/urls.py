61c4462e6ffce87ede134d00f1a467471720934d
Najiba Halim <najiba.r.halim@gmail.com>
2019-01-22 22:25:23 +0530

    test table

--- userinterface/urls.py
+++ userinterface/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('viewthings', views.viewthings, name='viewthings'),
-    # path('ctest', views.tst, name='tst'),
+    path('ctest/<slug:tableName>', views.tst, name='tst'),
     path('createSensor',views.createDevice,name='createDevice'),
     path('viewtable/<slug:tableName>',views.tableView, name="viewtable")
 ]
--- userinterface/views.py
+++ userinterface/views.py
@@ -7,46 +7,50 @@ from api.functions import API
 
 
 def dashboard(request):
-  response = requests.get('http://localhost:8000/api/getThings').json()
-  data = {}
-  data['count'] = response['count']
-  data['ThingData'] = response['data']
-  response = requests.get('http://localhost:8000/api/getThingType').json()
-  data['dataPoints'] = response['dataPoints']
-  data['listSize'] = len(response['dataPoints'])
-  data['title'] = response['title']
-  #print(str(data))
-  
-  return render(request, 'userinterface/dashboard.html',{'data':data})
+    response = requests.get('http://localhost:8000/api/getThings').json()
+    data = {}
+    data['count'] = response['count']
+    data['ThingData'] = response['data']
+    response = requests.get('http://localhost:8000/api/getThingType').json()
+    data['dataPoints'] = response['dataPoints']
+    data['listSize'] = len(response['dataPoints'])
+    data['title'] = response['title']
+    #print(str(data))
+
+    return render(request, 'userinterface/dashboard.html',{'data':data})
 
 def createDevice(request):
-  return render(request, 'userinterface/createdevice.html' )
+    return render(request, 'userinterface/createdevice.html' )
 
 
 def viewthings(request):
-  response = requests.get('http://localhost:8000/api/getThings').json()
-  data = {}
-  data['count'] = response['count']
-  data['ThingData'] = response['data']
-  response = requests.get('http://localhost:8000/api/getThingType').json()
-  data['dataPoints'] = response['dataPoints']
-  data['listSize'] = len(response['dataPoints'])
-  data['title'] = response['title']
-  print("___________________")
-  print(data['ThingData'])
-  return render(request, 'userinterface/viewthings.html', {'data': data['ThingData']})
-  
+    response = requests.get('http://localhost:8000/api/getThings').json()
+    data = {}
+    data['count'] = response['count']
+    data['ThingData'] = response['data']
+    response = requests.get('http://localhost:8000/api/getThingType').json()
+    data['dataPoints'] = response['dataPoints']
+    data['listSize'] = len(response['dataPoints'])
+    data['title'] = response['title']
+    print("___________________")
+    print(data['ThingData'])
+    return render(request, 'userinterface/viewthings.html', {'data': data['ThingData']})
 
 
-# def tst(request):
-#   hello.apply_async(args=[],countdown=3)
-#   return HttpResponse("WOOOWW")
+
+
+def tst(request,tableName):
+    response = requests.get('http://localhost:8000/api/makeTable/'+tableName).json()
+    print(response['Items'])
+    print(type(response['Items']))
+    response=json.loads(response['Items'])
+    return render(request, 'userinterface/test.html',{'data':response})
 
 def tableView(request,tableName):
-  obj = API()
-  data = {}
-  print(tableName)
-  data['data'] = obj.getTableData(tableName)
-
-  return render(request, 'userinterface/test.html',{'data':data['data']})
+    obj = API()
+    data = {}
+    print(tableName)
+    data['data'] = obj.getTableData(tableName)
+    
+    return render(request, 'userinterface/test.html',{'data':data['data']})
 
